c9826909a2d2322f992d8817a42543704c39b3e9
stian <stian.laate@gmail.com>
2019-06-27 22:32:26 +0200

    received call successfully on pi

--- backend/appserver.py
+++ backend/appserver.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
   from greenhouse.application import create_app
   app = create_app()
   app.run(host='0.0.0.0')
+
--- backend/greenhouse/api_pkg/flask_app.py
+++ backend/greenhouse/api_pkg/flask_app.py
@@ -6,15 +6,13 @@ flask_app.py
 from flask import Flask
 from flask_cors import CORS
 from .api import api
+import logging
 
 def create_flask_app(app_name='GREENHOUSE'):
-  print("0")
+  logging.info("starting flask app")
   flask_app = Flask(app_name)
-  print("1")  
   flask_app.config.from_object('greenhouse.api_pkg.config.BaseConfig')
-  print("2")
   cors = CORS(flask_app, resources={"/api/*": {"origins": "*"}})
-  print("3")  
   flask_app.register_blueprint(api, url_prefix="/api")
-  print("Started flask app")
+  logging.info("flask app started")
   return flask_app
