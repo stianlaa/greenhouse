df7cfd8e015d2277377864b430c3ab97ca88ff59
Stian Laate <stian.late@visma.com>
2019-07-18 10:52:40 +0200

    quick fix

--- backend/greenhouse/api_pkg/config.py
+++
@@ -1,7 +0,0 @@
-"""
-config.py  
-- settings for the flask application object
-"""
-
-class BaseConfig(object):  
-    DEBUG = False
--- backend/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""
-wsgi.py
-- should make the app object serve as callable for the uwsgi container server to execute against
-"""
-
-from greenhouse.application import create_app
-app = create_app()
